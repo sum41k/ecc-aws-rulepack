f17b5704f61cc0107b3ff86a8327f15a8f7fcc94
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-07-05 13:56:18 +0300

    new_rules_from_sprint

--- tests/ecc-aws-272-eliminate_use_root_user_for_administrative_and_daily_tasks/red_policy_test.py
+++ tests/ecc-aws-272-eliminate_use_root_user_for_administrative_and_daily_tasks/red_policy_test.py
@@ -1,8 +1,14 @@
+import datetime
+
 class PolicyTest(object):
+
+    def mock_time(self):
+        return 2023, 6, 21
 
     def test_resources(self, base_test, resources):
         base_test.assertEqual(len(resources), 1)
-        base_test.assertTrue(resources[0]['c7n:credential-report']['access_keys'][0]['last_used_date'])
-        base_test.assertTrue(resources[0]['c7n:credential-report']['password_last_used'])
 
-        
+        passwordLastUsed=datetime.datetime.fromisoformat(str(resources[0]['c7n:credential-report']['password_last_used']))
+        time_now = datetime.datetime.fromisoformat(datetime.datetime.utcnow().replace(microsecond=0).replace(tzinfo=datetime.timezone.utc).isoformat())
+        delta = time_now - passwordLastUsed
+        base_test.assertTrue(delta.days<90)

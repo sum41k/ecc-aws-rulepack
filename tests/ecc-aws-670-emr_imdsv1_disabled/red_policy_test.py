98f9de8edebc99c3977f20c280f8dbc53daec717
Vladyslav Yevsiukov <Vladyslav_Yevsiukov@epam.com>
2023-07-05 13:07:20 +0000

    update

--- tests/ecc-aws-670-emr_imdsv1_disabled/red_policy_test.py
+++ tests/ecc-aws-670-emr_imdsv1_disabled/red_policy_test.py
@@ -2,4 +2,7 @@ class PolicyTest(object):
 
     def test_resources_with_client(self, base_test, resources, local_session):
         base_test.assertEqual(len(resources), 1)
-        base_test.assertTrue(resources[0]['SecurityConfiguration']['InstanceMetadataServiceConfiguration']['MinimumInstanceMetadataServiceVersion'], 1)
+        emr_client = local_session.client("emr")
+        config_name = resources[0]['SecurityConfiguration']
+        configuration = emr_client.describe_security_configuration(Name=config_name)
+        base_test.assertRegexpMatches(configuration['SecurityConfiguration'], r'\"MinimumInstanceMetadataServiceVersion\": 1')

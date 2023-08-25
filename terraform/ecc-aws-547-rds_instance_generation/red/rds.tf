9f014d6d7b9d632892456a9fb8c2f43734bc8e4c
Vladyslav Yevsiukov <Vladyslav_Yevsiukov@epam.com>
2023-08-25 13:48:30 +0000

    new: added policy ecc-aws-547-rds_instance_generation

---
+++ terraform/ecc-aws-547-rds_instance_generation/red/rds.tf
@@ -0,0 +1,18 @@
+resource "random_password" "this" {
+  length           = 12
+  special          = true
+  numeric          = true
+  override_special = "!#$%*()-_=+[]{}:?"
+}
+
+resource "aws_db_instance" "this" {
+  allocated_storage     = 10
+  engine                = "mysql"
+  engine_version        = "5.7"
+  instance_class        = "db.t2.micro"
+  db_name               = "database547red"
+  username              = "root"
+  password              = random_password.this.result
+  parameter_group_name  = "default.mysql5.7"
+  skip_final_snapshot   = true
+}

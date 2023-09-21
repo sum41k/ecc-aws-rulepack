738f42bd048e3df080ca7ccce90a9da1c203ced6
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-09-21 11:26:22 +0300

    new: added policy ecc-aws-572-disabled_kms_keys_removed

---
+++ terraform/ecc-aws-572-disabled_kms_keys_removed/red/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.default-region
+
+  default_tags {
+    tags = {
+      CustodianRule    = "ecc-aws-572-disabled_kms_keys_removed"
+      ComplianceStatus = "Red"
+    }
+  }
+}

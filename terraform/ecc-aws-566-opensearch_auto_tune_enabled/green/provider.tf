113c7d80b2753114315ee4c84306b100dc2a74dc
Astr1k <Vladyslav_Yevsiukov@epam.com>
2023-11-27 15:13:54 +0000

    new: added policy ecc-aws-566-opensearch_auto_tune_enabled

---
+++ terraform/ecc-aws-566-opensearch_auto_tune_enabled/green/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.default-region
+
+  default_tags {
+    tags = {
+      CustodianRule    = "ecc-aws-566-opensearch_auto_tune_enabled"
+      ComplianceStatus = "Green"
+    }
+  }
+}

44718653034439748bb490bd36e99e81f8113bc1
Astr1k <Vladyslav_Yevsiukov@epam.com>
2023-11-29 09:03:03 +0000

    new: added policy ecc-aws-602-cloudwatch_logs_with_no_log_retention_period

---
+++ terraform/ecc-aws-602-cloudwatch_logs_with_no_log_retention_period/green/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
+    }
+  }
+}
+
+provider "aws"{
+  profile = var.profile
+  region = var.default-region
+
+  default_tags {
+    tags = {
+      CustodianRule    = "ecc-aws-602-cloudwatch_logs_with_no_log_retention_period"
+      ComplianceStatus = "Green"
+    }
+  }
+}

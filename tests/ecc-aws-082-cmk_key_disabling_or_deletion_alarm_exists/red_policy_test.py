743ef1572b8a955e12ce1601b6aba83d8c10d194
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-12-16 18:32:55 +0200

    new: added policy ecc-aws-082-cmk_key_disabling_or_deletion_alarm_exists

---
+++ tests/ecc-aws-082-cmk_key_disabling_or_deletion_alarm_exists/red_policy_test.py
@@ -0,0 +1,25 @@
+from botocore.exceptions import ClientError
+
+class PolicyTest(object):
+
+    def test_resources_with_client(self, base_test, resources, local_session):
+        base_test.assertEqual(len(resources), 1)
+
+        trail_client = local_session.client("cloudtrail")
+        trail_name = "c7n-082-cloudtrail-red"
+        describe_trails = trail_client.describe_trails(trailNameList=[trail_name])
+        base_test.assertFalse(describe_trails["trailList"][0]["IsMultiRegionTrail"])
+        base_test.assertTrue(describe_trails["trailList"][0]["IncludeGlobalServiceEvents"])
+
+        trail_status = trail_client.get_trail_status(Name=trail_name)
+        base_test.assertTrue(trail_status["IsLogging"])
+
+        event_selectors = trail_client.get_event_selectors(TrailName=trail_name)
+        base_test.assertEqual(event_selectors["EventSelectors"][0]["ReadWriteType"], "All")
+        base_test.assertTrue(event_selectors["EventSelectors"][0]["IncludeManagementEvents"])
+
+        logs_client = local_session.client("logs")
+        try:
+          logs_metrics = logs_client.describe_metric_filters(logGroupName="082_log_group_red1")
+        except ClientError as e:
+          base_test.assertEqual(e.response['Error']['Code'],"ResourceNotFoundException")

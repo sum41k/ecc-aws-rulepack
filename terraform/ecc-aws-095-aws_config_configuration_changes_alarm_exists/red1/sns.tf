710bdbb3948b3f938a470bf4e88650d53c5b8f65
Anna Shcherbak <Anna_Shcherbak@epam.com>
2023-12-16 19:39:20 +0200

    new: added policy ecc-aws-095-aws_config_configuration_changes_alarm_exists

--- terraform/ecc-aws-095-aws_config_configuration_changes_alarm_exists/red1/sns.tf
+++ terraform/ecc-aws-095-aws_config_configuration_changes_alarm_exists/red1/sns.tf
@@ -2,16 +2,12 @@ resource "aws_sns_topic" "this" {
   name = "095_sns_red"
 }
 
-resource "null_resource" "this" {
-  provisioner "local-exec" {
-    command = join(" ", [
-      "aws sns subscribe",
-      "--topic-arn ${aws_sns_topic.this.arn}",
-      "--protocol email",
-      "--notification-endpoint ${var.test-email}",
-      "--profile ${var.profile}",
-      "--region ${var.default-region}"
-      ]
-    )
-  }
+resource "aws_sqs_queue" "this" {
+  name = "095-sqs-red"
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.this.arn
 }

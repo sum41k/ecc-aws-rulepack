4e3e5ff82d4cc11ccf707f7818c3ba3d7a4e43fe
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-01-01 21:34:10 +0200

    new: added policy ecc-aws-096-security_group_changes_alarm_exists

--- terraform/ecc-aws-096-security_group_changes_alarm_exists/red1/sns.tf
+++ terraform/ecc-aws-096-security_group_changes_alarm_exists/red1/sns.tf
@@ -2,17 +2,6 @@ resource "aws_sns_topic" "this" {
   name = "096-sns-red1"
 }
 
-resource "null_resource" "this" {
-  provisioner "local-exec" {
-    command = join(" ", [
-      "aws sns subscribe",
-      "--topic-arn ${aws_sns_topic.this.arn}",
-      "--protocol email",
-      "--notification-endpoint ${var.test-email}",
-      "--profile ${var.profile}",
-      "--region ${var.default-region}"
-
-      ]
-    )
-  }
+resource "aws_sqs_queue" "this" {
+  name = "096-sqs-red"
 }

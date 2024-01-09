a47b972645c86df636563e4b0f94f744a5bde0f1
Astr1k <Vladyslav_Yevsiukov@epam.com>
2024-01-09 15:02:32 +0000

    new: added policy ecc-aws-593-reserved_rds_instance_recent_purchases

---
+++ tests/ecc-aws-593-reserved_rds_instance_recent_purchases/red_policy_test.py
@@ -0,0 +1,15 @@
+import datetime
+from dateutil import tz as tzutil
+
+class PolicyTest(object):
+
+    def mock_time(self):
+        return 2024, 1, 9
+        
+    def test_resources(self, base_test, resources):
+        base_test.assertEqual(len(resources), 1)
+        base_test.assertEqual(resources[0]['State'], "active")
+        start_time = resources[0]["StartTime"]
+        time_now = datetime.datetime.fromisoformat(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat())
+        delta = start_time - time_now
+        base_test.assertTrue(delta.days<=7)

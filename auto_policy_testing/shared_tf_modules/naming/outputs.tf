c21fc344c7d2d9190e47f1fbfa2381f954df19ae
Stanislav Babkin <stanislav_babkin@epam.com>
2024-04-24 08:33:26 +0300

    skip: tf templates for event-bus testing developed; resources deplopyed and tested by custodian

--- auto_policy_testing/shared_tf_modules/naming/outputs.tf
+++ auto_policy_testing/shared_tf_modules/naming/outputs.tf
@@ -1,5 +1,6 @@
 output "resource_prefix" {
   value = {
+    event_bus         = "${local.suffix}_${var.resource_type}_event_bus_${local.compliance_status}"
     firehose          = "${local.suffix}_${var.resource_type}_firehose_${local.compliance_status}" 
     efs               = "${local.suffix}_${var.resource_type}_efs_${local.compliance_status}"
     dlm_policy        = "${local.suffix}_${var.resource_type}_dlm_policy_${local.compliance_status}"
